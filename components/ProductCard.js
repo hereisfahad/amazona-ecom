3c1342b193d76cdb205dcce9120a15d2d833a057
hereisfahad <fahadjabbar31@gmail.com>
2020-12-22 00:30:32 +0500

    design cart page

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,10 +1,10 @@
-import { Box, Image, Text, Icon, Flex} from '@chakra-ui/react';
+import { Box, Img, Text, Icon, Flex} from '@chakra-ui/react';
 import { FaStar, FaStarHalfAlt } from "react-icons/fa";
 
 const ProductCard = () => {
     return (
         <Box borderWidth="1px" borderColor="gray.200" rounded="md" bg="gray.50">
-            <Image src="https://bit.ly/sage-adebayo" alt="Segun Adebayo" maxWidth="16rem" width="100%" rounded="md" borderBottomRadius="none"/>
+            <Img src="https://bit.ly/sage-adebayo" alt="Segun Adebayo" maxWidth="16rem" width="100%" rounded="md" borderBottomRadius="none"/>
          <Box display="flex" flexDirection="column" p={4}>
             <Text as="h2" fontSize="lg" mb={3}>Nike Slim Shirts</Text>
             <Flex mb={1}>
