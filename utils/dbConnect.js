3ae8611e77d2529ac04434cbe9aaf59ad98f74fe
hereisfahad <fahadjabbar31@gmail.com>
2020-12-29 01:27:45 +0500

    add db connection

---
+++ utils/dbConnect.js
@@ -0,0 +1,21 @@
+/* This is a database connection function*/
+import mongoose from 'mongoose'
+import '@/models/Product'
+
+const connection = {} /* creating connection object*/
+
+async function dbConnect() {
+  /* check if we have connection to our databse*/
+  if (connection.isConnected) return
+
+  /* connecting to our database */
+  const db = await mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+
+  connection.isConnected = db.connections[0].readyState
+}
+
+export default dbConnect
