302610a36a4a071d4acff22093bb1a31a87d66e3
hereisfahad <fahadjabbar31@gmail.com>
2020-12-29 23:45:24 +0500

    fix: cannot overwrite model once compiled mongoose

--- models/Product.js
+++ models/Product.js
@@ -17,4 +17,4 @@ const ProductSchema = new mongoose.Schema(
     }
 )
 
-export default mongoose.model('Product', ProductSchema)
+export default mongoose.models.Product || mongoose.model('Product', ProductSchema)
