1f8ad1051785dd1bf9eaf696d3e54751dc97be86
hereisfahad <fahadjabbar31@gmail.com>
2020-12-29 23:49:43 +0500

    fetch and display products from db

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,12 +1,13 @@
 import { Box, Img, Text, Icon, Flex} from '@chakra-ui/react';
 import { FaStar, FaStarHalfAlt } from "react-icons/fa";
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
+    const { name, image, price } = product
     return (
         <Box borderWidth="1px" borderColor="gray.200" rounded="md" bg="gray.50">
-            <Img src="https://bit.ly/sage-adebayo" alt="Segun Adebayo" maxWidth="16rem" width="100%" rounded="md" borderBottomRadius="none"/>
+            <Img src={image} alt="product image" maxWidth="16rem" width="100%" rounded="md" borderBottomRadius="none"/>
          <Box display="flex" flexDirection="column" p={4}>
-            <Text as="h2" fontSize="lg" mb={3}>Nike Slim Shirts</Text>
+            <Text as="h2" fontSize="lg" mb={3}>{name}</Text>
             <Flex mb={1}>
                 <Icon as={FaStar} color="yellow.500"/>
                 <Icon as={FaStar} color="yellow.500"/>
@@ -14,7 +15,7 @@ const ProductCard = () => {
                 <Icon as={FaStar} color="yellow.500"/>
                 <Icon as={FaStarHalfAlt} color="yellow.500"/>
             </Flex>
-            <Text fontSize="lg" mb={1}>$120</Text>
+            <Text fontSize="lg" mb={1}>${price}</Text>
          </Box>
         </Box>
     );
--- pages/products.js
+++ pages/products.js
@@ -1,21 +1,27 @@
 import { Text, SimpleGrid } from "@chakra-ui/react";
+import useSWR from 'swr';
+import fetcher from '@/utils/fetcher';
 
 import Page from '@/components/Page';
 import DashboardShell from '@/components/DashboardShell';
 import ProductCard from '@/components/ProductCard'
 
 const Products = () => {
+    const { data } = useSWR(`/api/products`, fetcher);
     return (
         <DashboardShell>
             <Text as="h1" color="secondary" textAlign="center" fontSize="4xl">
                 List of products
             </Text>
-            <SimpleGrid 
-            columns={[2, null, 3]} 
-            spacing={10} mt={2}>
-                <ProductCard/>
-                <ProductCard/>
-                <ProductCard/>
+            <SimpleGrid
+                columns={[2, null, 3]}
+                spacing={10} mt={2}
+            >
+                {
+                    data && data?.products.map(product => {
+                        return <ProductCard key={product._id} product={product} />
+                    })
+                }
             </SimpleGrid>
         </DashboardShell>
     )
