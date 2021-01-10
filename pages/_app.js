1099eeca355faeae696d5bf8eeed6ba2d8d033dc
hereisfahad <fahadjabbar31@gmail.com>
2021-01-11 01:38:53 +0500

    feat: add cart provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import CartProvider from '@/providers/cart'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider resetCSS>
+      <CartProvider>
         <Component {...pageProps} />
+      </CartProvider>
     </ChakraProvider>
   )
 }
--- pages/product/[productId].js
+++ pages/product/[productId].js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
     Text,
     SimpleGrid,
@@ -5,21 +6,35 @@ import {
     Flex,
     List,
     ListItem,
-    Button
+    Button,
+    NumberInput,
+    NumberInputField,
+    NumberInputStepper,
+    NumberIncrementStepper,
+    NumberDecrementStepper,
 } from "@chakra-ui/react";
 import useSWR from 'swr';
 import fetcher from '@/utils/fetcher';
 import { useRouter } from 'next/router';
+import { useCart } from '@/providers/cart';
 
 import Page from '@/components/Page';
 import DashboardShell from '@/components/DashboardShell';
 import Rating from '@/components/Rating';
 
 const Product = () => {
-    const { query } = useRouter();
-    const { data } = useSWR(`/api/products?_id=${query.productId}`, fetcher);
-    if(!data) return 'loading...'
+    const [quantity, setQuantity] = useState(1);
+    const router = useRouter()
+    const { data } = useSWR(`/api/products?_id=${router.query.productId}`, fetcher);
+    if (!data) return 'loading...'
     const { image, name, rating, numReviews, price, description, countInStock } = data?.products[0]
+    const { addItem } = useCart()
+
+    const handleAddToCart = () => {
+        addItem(data?.products[0], Number(quantity))
+        router.push('/cart')
+    }
+
     return (
         <DashboardShell>
             <Text as="h1" color="secondary" textAlign="center" fontSize="4xl">
@@ -32,29 +47,56 @@ const Product = () => {
                 <Flex direction="column">
                     <Text fontWeight="bold" mb={4} fontSize="lg">{name}</Text>
                     <Flex mb={1} alignItems="center">
-                        <Rating rating={rating} mr={2}/>
+                        <Rating rating={rating} mr={2} />
                         <Text fontSize="lg" >{numReviews} reviews</Text>
                     </Flex>
                     <Text mb={1} fontWeight="500">Price: ${price}</Text>
                     <Text fontWeight="500">Description: {description}</Text>
                 </Flex>
-                <Flex direction="column" bg="gray.100" rounded="md" p={4} h="9rem" border="1px solid" borderColor="gray.300">
+                <Flex direction="column" bg="gray.100" rounded="md" p={4} maxHeight="11rem" border="1px solid" borderColor="gray.300">
                     <List mb={2}>
                         <ListItem display="flex" fontWeight="bold" justifyContent="space-between" mb={1}>
                             <Text>Price</Text> <Text fontSize="lg">${price}</Text>
                         </ListItem>
                         <ListItem display="flex" fontWeight="bold" justifyContent="space-between" mb={1}>
-                            <Text>Status</Text> 
+                            <Text>Status</Text>
                             {
                                 countInStock > 0 ? (
                                     <Text color="green.500">In Stock</Text>
-                                ): (
-                                    <Text color="red.500">Not available</Text>
-                                )
+                                ) : (
+                                        <Text color="red.500">Not available</Text>
+                                    )
                             }
                         </ListItem>
+                        {
+                            countInStock > 0 && (
+                                <ListItem display="flex" fontWeight="bold" justifyContent="space-between" mb={1}>
+                                    <Text>Quantity</Text>
+                                    <NumberInput
+                                        size="sm"
+                                        w="65px"
+                                        step={1}
+                                        defaultValue={1}
+                                        min={1}
+                                        max={countInStock}
+                                        onChange={(value) => setQuantity(value)}
+                                    >
+                                        <NumberInputField />
+                                        <NumberInputStepper>
+                                            <NumberIncrementStepper />
+                                            <NumberDecrementStepper />
+                                        </NumberInputStepper>
+                                    </NumberInput>
+                                </ListItem>
+                            )
+                        }
                     </List>
-                    <Button bg="yellow.400" rounded="md" disabled={countInStock === 0}>
+                    <Button
+                        bg="yellow.400"
+                        rounded="md"
+                        disabled={countInStock === 0}
+                        onClick={handleAddToCart}
+                    >
                         Add to Cart
                     </Button>
                 </Flex>
