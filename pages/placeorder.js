9c4a545728b1f5d62b9fa099bd07c5912cda4ccb
hereisfahad <fahadjabbar31@gmail.com>
2021-01-15 00:15:53 +0500

    make placeorder page responsive

--- pages/placeorder.js
+++ pages/placeorder.js
@@ -4,14 +4,15 @@ import {
     List,
     ListItem,
     Button,
-    Grid,
-    GridItem,
+    Box,
+    SimpleGrid,
     Img,
 } from "@chakra-ui/react";
 import { useRouter } from 'next/router'
 
 import Page from '@/components/Page';
 import DashboardShell from '@/components/DashboardShell';
+import { Table, Tr, Th, Td } from '@/components/Table';
 import { useOrder } from '@/providers/order';
 import { useCart } from '@/providers/cart';
 
@@ -40,21 +41,18 @@ const PlaceOrder = () => {
             <Text as="h1" color="secondary" textAlign="center" fontSize="4xl">
                 Order Detail
             </Text>
-            <Grid
-                w="80vw"
-                maxW="90vw"
-                templateRows="repeat(3, 1fr)"
-                templateColumns="repeat(5, 1fr)"
-                gap={4}
+            <SimpleGrid
+                columns={[1, 1, 2]}
+                spacing={[0, 0, 10]}
             >
-
-                {/* shipping*/}
-                <GridItem rowSpan={1} colSpan={[5, 5, 3]}>
+                <Flex direction="column">
+                    {/* shipping*/}
                     <Flex
                         direction="column"
                         bg="gray.100"
                         rounded="md"
-                        p={4}
+                        p={[2, 4, 4]}
+                        mb={2}
                         border="1px solid"
                         borderColor="gray.300"
                     >
@@ -70,15 +68,14 @@ const PlaceOrder = () => {
                             </ListItem>
                         </List>
                     </Flex>
-                </GridItem>
 
-                {/* payment*/}
-                <GridItem rowSpan={1} colSpan={[5, 5, 3]}>
+                    {/* payment*/}
                     <Flex
                         direction="column"
                         bg="gray.100"
                         rounded="md"
-                        p={4}
+                        p={[2, 4, 4]}
+                        mb={2}
                         border="1px solid"
                         borderColor="gray.300"
                     >
@@ -91,71 +88,81 @@ const PlaceOrder = () => {
                             </ListItem>
                         </List>
                     </Flex>
-                </GridItem>
 
-                {/* order items*/}
-                <GridItem rowSpan={1} colSpan={[5, 5, 3]}>
+                    {/* order items*/}
                     <Flex
                         direction="column"
                         bg="gray.100"
                         rounded="md"
-                        p={4}
+                        p={[2, 4, 4]}
+                        mb={2}
                         border="1px solid"
                         borderColor="gray.300"
                     >
                         <Text fontWeight="bold" mb={2}>Order Items</Text>
-                        <List mb={2}>
-                            {cartItems.map((item, index) => (
-                                <ListItem display="flex" justifyContent="space-between" mb={1}>
-                                    <Img src={item.image} alt="Segun Adebayo" maxWidth="4rem" width="100%" rounded="md" />
-                                    <Text>{item.name}</Text>
-                                    <Text>{item.quantity} x ${item.price} = ${item.quantity * item.price}</Text>
-                                </ListItem>
-                            ))}
-                        </List>
+                        <Box overflowX="scroll">
+                            <Table w="full">
+                                <thead>
+                                    <Tr>
+                                        <Th>Image</Th>
+                                        <Th>Name</Th>
+                                        <Th>Price</Th>
+                                    </Tr>
+                                </thead>
+                                <tbody>
+                                    {cartItems.map((item, index) => (
+                                        <Box as="tr" key={index}>
+                                            <Td>
+                                                <Img src={item.image} alt="Segun Adebayo" maxWidth="4rem" width="100%" rounded="md" />
+                                            </Td>
+                                            <Td>{item.name}</Td>
+                                            <Td>{item.quantity} x ${item.price} = ${item.quantity * item.price}</Td>
+                                        </Box>
+                                    ))}
+                                </tbody>
+                            </Table>
+                        </Box>
                     </Flex>
-                </GridItem>
+                </Flex>
 
                 {/* order summary*/}
-                <GridItem rowSpan={2} colSpan={[5, 5, 2]} rowStart={["4", "4", "1"]} colStart={["1", "1", "4"]}>
-                    <Flex
-                        direction="column"
-                        bg="gray.100"
+                <Flex
+                    direction="column"
+                    bg="gray.100"
+                    rounded="md"
+                    p={[2, 4, 4]}
+                    h="14rem"
+                    border="1px solid"
+                    borderColor="gray.300"
+                >
+                    <Text fontWeight="bold" mb={2}>Order Summary</Text>
+                    <List mb={2}>
+                        <ListItem display="flex" flexDirection="column" justifyContent="space-between" mb={1}>
+                            <Flex justifyContent="space-between">
+                                <Text>Items: </Text> <Text fontSize="lg">${cartTotal.toFixed(2)}</Text>
+                            </Flex>
+                            <Flex justifyContent="space-between">
+                                <Text>Shipping: </Text> <Text fontSize="lg">${ShippingCost.toFixed(2)}</Text>
+                            </Flex>
+                            <Flex justifyContent="space-between">
+                                <Text>Tax: </Text> <Text fontSize="lg">${TaxCost.toFixed(2)}</Text>
+                            </Flex>
+                            <Flex justifyContent="space-between" fontWeight="semibold">
+                                <Text>Order Total: </Text> <Text fontSize="lg">${(cartTotal + ShippingCost + TaxCost).toFixed(2)}</Text>
+                            </Flex>
+                        </ListItem>
+                    </List>
+                    <Button
+                        bg="yellow.400"
                         rounded="md"
-                        p={4}
-                        h="14rem"
-                        border="1px solid"
-                        borderColor="gray.300"
+                        mt="auto"
+                        onClick={handleSubmit}
                     >
-                        <Text fontWeight="bold" mb={2}>Order Summary</Text>
-                        <List mb={2}>
-                            <ListItem display="flex" flexDirection="column" justifyContent="space-between" mb={1}>
-                                <Flex justifyContent="space-between">
-                                    <Text>Items: </Text> <Text fontSize="lg">${cartTotal.toFixed(2)}</Text>
-                                </Flex>
-                                <Flex justifyContent="space-between">
-                                    <Text>Shipping: </Text> <Text fontSize="lg">${ShippingCost.toFixed(2)}</Text>
-                                </Flex>
-                                <Flex justifyContent="space-between">
-                                    <Text>Tax: </Text> <Text fontSize="lg">${TaxCost.toFixed(2)}</Text>
-                                </Flex>
-                                <Flex justifyContent="space-between" fontWeight="semibold">
-                                    <Text>Order Total: </Text> <Text fontSize="lg">${(cartTotal + ShippingCost + TaxCost).toFixed(2)}</Text>
-                                </Flex>
-                            </ListItem>
-                        </List>
-                        <Button
-                            bg="yellow.400"
-                            rounded="md"
-                            mt="auto"
-                            onClick={handleSubmit}
-                        >
-                            Place Order
+                        Place Order
                     </Button>
-                    </Flex>
-                </GridItem>
+                </Flex>
 
-            </Grid>
+            </SimpleGrid>
 
         </DashboardShell>
     )
