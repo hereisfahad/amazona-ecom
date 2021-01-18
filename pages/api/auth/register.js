e5d37e7cd3130d86e9d0c92aa2e86eb379239fd6
hereisfahad <fahadjabbar31@gmail.com>
2021-01-18 16:20:22 +0500

    integerate jsonwebtoken and gravatar

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -1,11 +1,22 @@
 import dbConnect from '@/utils/dbConnect'
 import User from '@/models/User'
+import gravatar from 'gravatar'
+import jwt from 'jsonwebtoken'
 
 export default async function handler(req, res) {
-  await dbConnect()
+    await dbConnect()
     try {
-        const user = await User.create(req.body) 
-        res.status(201).json({ success: true, user })
+        const avatar = gravatar.url(req.body.email, { s: '200', r: 'pg', d: 'mm' });
+        const user = await User.create({ ...req.body, image: avatar  })
+        const payload = {
+            user: { id: user.id }
+        }
+        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: 3600 })
+        const { _id, name, email, isAdmin, image } = user
+        res.status(201).json({
+            success: true,
+            user: { _id, name, email, isAdmin, image, token }
+        })
     } catch (error) {
         res.status(400).json({ success: false, message: `couldn't register user` })
     }
--- pages/api/auth/signin.js
+++ pages/api/auth/signin.js
@@ -1,15 +1,24 @@
 import dbConnect from '@/utils/dbConnect'
 import User from '@/models/User'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 export default async function handler(req, res) {
     await dbConnect()
     const { email, password } = req.body
     try {
         const user = await User.findOne({ email })
-        if(user) {
-            if(bcrypt.compareSync(password, user.password)) {
-                return res.status(200).json({ success: true, user })
+        if (user) {
+            if (bcrypt.compareSync(password, user.password)) {
+                const payload = {
+                    user: { id: user.id }
+                }
+                const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: 3600 })
+                const { _id, name, email, isAdmin, image } = user
+                return res.status(200).json({
+                    success: true,
+                    user: { _id, name, email, isAdmin, image, token }
+                })
             }
         }
         return res.status(401).json({ success: false, message: "Invalid email or password." })
