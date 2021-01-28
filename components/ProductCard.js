25cdaab7ef7d2db4634b198535c7d0a1769f75ba
hereisfahad <fahadjabbar31@gmail.com>
2021-01-29 00:42:42 +0500

    add loading skeleton of  product image

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,20 +1,38 @@
-import { Box, Img, Text, Flex} from '@chakra-ui/react';
+import { useState } from 'react';
+import { Box, Img, Text, Flex, Skeleton } from '@chakra-ui/react';
 
 import Rating from '@/components/Rating';
+
+const ImageWithSkeleton = ({ image }) => {
+    const [loaded, setLoaded] = useState(false);
+    return (
+        <Skeleton isLoaded={loaded}>
+            <Img 
+                src={image}
+                alt="product image"
+                width="300px"
+                height="300px"
+                rounded="md"
+                borderBottomRadius="none"
+                onLoad={() => setLoaded(true)}
+            />
+        </Skeleton>
+    )
+}
 
 const ProductCard = ({ product }) => {
     const { name, image, rating, numReviews, price } = product
     return (
         <Box borderWidth="1px" borderColor="gray.200" rounded="md" bg="gray.50">
-            <Img src={image} alt="product image" maxWidth="16rem" width="100%" rounded="md" borderBottomRadius="none"/>
-         <Box display="flex" flexDirection="column" p={4}>
-            <Text as="h2" fontSize="lg" mb={3}>{name}</Text>
-            <Flex alignItems="center">
-                <Rating rating={rating} mr={2}/>
-                <Text fontSize="lg" >{numReviews} reviews</Text>
-            </Flex>
-            <Text fontSize="lg" mb={1}>${price}</Text>
-         </Box>
+            <ImageWithSkeleton image={image} />
+            <Box display="flex" flexDirection="column" p={4}>
+                <Text as="h2" fontSize="lg" mb={3}>{name}</Text>
+                <Flex alignItems="center">
+                    <Rating rating={rating} mr={2}/>
+                    <Text fontSize="lg" >{numReviews} reviews</Text>
+                </Flex>
+                <Text fontSize="lg" mb={1}>${price}</Text>
+            </Box>
         </Box>
     );
 };
