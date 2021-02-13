043f8e77119fe8190d9803c7b6a75d5a1c4b9c86
hereisfahad <fahadjabbar31@gmail.com>
2021-02-14 02:14:21 +0500

    add share product button

--- pages/product/[productId].js
+++ pages/product/[productId].js
@@ -12,11 +12,13 @@ import {
     NumberInputStepper,
     NumberIncrementStepper,
     NumberDecrementStepper,
+    useToast
 } from "@chakra-ui/react";
 import useSWR from 'swr';
 import fetcher from '@/utils/fetcher';
 import { useRouter } from 'next/router';
 import { useCart } from '@/providers/cart';
+import { RiShareForwardFill } from 'react-icons/ri';
 
 import Page from '@/components/Page';
 import DashboardShell from '@/components/DashboardShell';
@@ -24,6 +26,7 @@ import Rating from '@/components/Rating';
 import ProductDetailSkeleton from '@/skeletons/ProductDetailSkeleton';
 
 const Product = () => {
+    const toast = useToast()
     const [quantity, setQuantity] = useState(1);
     const [loading, setLoading] = useState(false)
     const router = useRouter()
@@ -37,6 +40,38 @@ const Product = () => {
         addItem(data?.products[0], Number(quantity))
         router.push('/cart')
     }
+
+    const shareProduct = () => {
+        const productURL = window.location.href;
+        const mobileRegExp = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+        const isMobile = mobileRegExp.test(navigator.userAgent);
+
+        if (isMobile) {
+            navigator
+                .share(productURL)
+                .then(() => {
+                    toast({
+                        description: "Copied to clipboard",
+                        status: "success",
+                        duration: 2000,
+                        isClosable: false,
+                    })
+                })
+                .catch((e) => console.log(e));
+        } else {
+            navigator.clipboard
+                .writeText(productURL)
+                .then(() => {
+                    toast({
+                        description: "Copied to clipboard",
+                        status: "success",
+                        duration: 2000,
+                        isClosable: false,
+                    })
+                })
+                .catch((e) => console.log(e));
+        }
+    };
 
     return (
         <DashboardShell>
@@ -61,7 +96,7 @@ const Product = () => {
                                 <Text mb={1} fontWeight="500">Price: ${product.price}</Text>
                                 <Text fontWeight="500">Description: {product.description}</Text>
                             </Flex>
-                            <Flex direction="column" bg="gray.100" rounded="md" p={4} maxHeight="11rem" border="1px solid" borderColor="gray.300">
+                            <Flex direction="column" bg="gray.100" rounded="md" p={4} maxHeight="13rem" border="1px solid" borderColor="gray.300">
                                 <List mb={2}>
                                     <ListItem display="flex" fontWeight="bold" justifyContent="space-between" mb={1}>
                                         <Text>Price</Text> <Text fontSize="lg">${product.price}</Text>
@@ -104,9 +139,17 @@ const Product = () => {
                                     rounded="md"
                                     disabled={product.countInStock === 0}
                                     isLoading={loading}
+                                    mb={2}
                                     onClick={handleAddToCart}
                                 >
                                     Add to Cart
+                                </Button>
+                                <Button
+                                    bg="yellow.400"
+                                    rounded="md"
+                                    onClick={shareProduct}
+                                >
+                                    <Text mr={2}>Share</Text><RiShareForwardFill w={4} h={4} />
                                 </Button>
                             </Flex>
                         </>
@@ -120,7 +163,7 @@ const Product = () => {
 const ProductPage = ({ product }) => {
     const { _id, name, description, image } = product
     return (
-        <Page 
+        <Page
             name="Product"
             path={`/product/${_id}`}
             ogTitle={name}
@@ -136,11 +179,11 @@ export default ProductPage
 
 export const getServerSideProps = async ({ query }) => {
     const response = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/products?_id=${query.productId}`)
-    const data =  await response.json()
+    const data = await response.json()
 
     return {
-      props: {
-        product: data.products[0],
-      },
+        props: {
+            product: data.products[0],
+        },
     };
 };
