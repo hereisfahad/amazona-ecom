0565ad19c2aa45dcc09376041a54c21c56adde40
hereisfahad <fahadjabbar31@gmail.com>
2021-02-13 19:40:46 +0500

    add progress bar on route loading

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,16 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import Router from 'next/router'
+import NProgress from 'nprogress'
+import '../public/nprogress.css'
+
 import CartProvider from '@/providers/cart'
 import OrderProvider from '@/providers/order'
+
+NProgress.configure({ showSpinner: false });
+
+Router.events.on('routeChangeStart', (url) => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp({ Component, pageProps }) {
   return (
